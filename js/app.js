fc9cfe6a3d5b32a1873fe8584db2cd9eaaddf1b9
luizbearzi <94871271+luizbearzi@users.noreply.github.com>
2023-11-25 02:17:54 -0300

    Uso do if e else para implementar alteração de status do jogo

--- js/app.js
+++ js/app.js
@@ -2,8 +2,15 @@ function alterarStatus(id) {
     let clicaBotao = document.getElementById(`game-${id}`);
     let imagem = clicaBotao.querySelector('.dashboard__item__img');
     let botao = clicaBotao.querySelector('.dashboard__item__button');
-    let nomeJogo = clicaBotao.querySelector('.dashboard__item__name');
 
-    alert(` fazendo solo e foi o nome é ${nomeJogo.textContent}`);
+   if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+   } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+   }
 
 }
